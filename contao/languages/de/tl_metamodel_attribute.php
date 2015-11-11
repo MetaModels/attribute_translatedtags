4737a76ddd58069038e6d2bb7093cf5128c441b2
Mini Model <minimodel@metamodel.me>
2015-11-11 16:16:07 +0100

    Updated translations from transifex.

--- contao/languages/de/tl_metamodel_attribute.php
+++ contao/languages/de/tl_metamodel_attribute.php
@@ -8,13 +8,14 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T23:10:20+02:00
+ * last-updated: 2015-07-11T07:34:34+02:00
  */
 
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_langcolumn']['0']           = 'Sprachenspalte';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_langcolumn']['1']           = 'Bitte angeben, welche Spalte den Sprachcode nach ISO 639-1 enthält.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srcsorting']['0']           = 'Sortierspalte';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srcsorting']['1']           = 'Wählen Sie bitte, welche Spalte der noch nicht übersetzten Tabelle für die Sortierung verwendet werden soll.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srctable']['0']             = 'Nicht übersetzte Tabelle für die Sortierung.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srctable']['0']             = 'Nicht-Übersetzte Tabelle für die Sortierung.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srctable']['1']             = 'Bitte nennen Sie die Tabelle welche die zu sortieren de Spalte enthält.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedtags'] = 'Übersetzte Tags';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedtags'] = 'Übersetzte Mehrfachauswahl';
+
