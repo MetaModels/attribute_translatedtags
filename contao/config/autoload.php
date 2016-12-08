d851d7fbe01303040adf7c6b9a8a7e81add49c0c
Sven Baumann <baumann.sv@gmail.com>
2016-12-08 17:24:09 +0100

    update header php doc and add license

--- contao/config/autoload.php
+++ contao/config/autoload.php
@@ -1,11 +1,17 @@
 <?php
+
 /**
+ * This file is part of MetaModels/attribute_translatedtags.
+ *
+ * (c) 2012-2016 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
  * The MetaModels extension allows the creation of multiple collections of custom items,
  * each with its own unique set of selectable attributes, with attribute extendability.
  * The Front-End modules allow you to build powerful listing and filtering of the
  * data in each collection.
- *
- * PHP version 5
  *
  * @package    MetaModels
  * @subpackage AttributeTranslatedTags
@@ -13,8 +19,9 @@
  * @author     Andreas Isaak <info@andreas-isaak.de>
  * @author     Christian de la Haye <service@delahaye.de>
  * @author     David Maack <david.maack@arcor.de>
- * @copyright  The MetaModels team.
- * @license    LGPL.
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2012-2016 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_translatedtags/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
 
